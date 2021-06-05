cb670f53a66019ba41a2bed534af4344adbbbe10
wangan <975742711@qq.com>
2021-06-06 01:04:47 +0800

    修改配置信息

--- mall_mbg/src/test/java/com/exp/success/mall_mbg/MallMbgApplicationTests.java
+++ mall_mbg/src/test/java/com/exp/success/mall_mbg/MallMbgApplicationTests.java
@@ -1,13 +1,13 @@
-package com.exp.success.mall_mbg;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MallMbgApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.exp.success.mall_mbg;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MallMbgApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
