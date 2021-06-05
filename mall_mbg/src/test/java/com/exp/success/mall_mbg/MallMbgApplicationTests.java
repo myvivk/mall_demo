62a057c1d5570794cc3867b41c22465f674a67b0
wangan <975742711@qq.com>
2021-06-06 00:51:56 +0800

    #

---
+++ mall_mbg/src/test/java/com/exp/success/mall_mbg/MallMbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp.success.mall_mbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
